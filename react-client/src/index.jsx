7e31a654dd1396058bf878c4fc45d28ea46dc405
rimamenaa <rimamenaa98@gmail.com>
2021-10-06 20:47:19 +0100

    changes

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,29 +6,24 @@ import Post from './components/Post.jsx';
 import Preview from './components/Preview.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
-
+import HomePage from './components/HomePage.jsx';
 /*
   READ THESE COMMENTS AS A PART OF STEP TWO
-
   To manage switching among the different views in this application, 
   we have implemented a "view switcher" in the `App` component. 
-
   There are three key parts to the view switcher:
     1. The `view` property defined on the `App` component's `state`
     2. The `changeView` method defined on the `App` component
     3. The `renderView` method defined on the `App` component
-
   The value of the `view` property will determine which gets returned by the
   `renderView` method, which is invoked inside the `App` component's `render`. 
   You can set the initial value of `view` in the `App` component's `constructor`
   function, determining what view gets rendered "by default".
-
   If you haven't modified this code yet, the view switcher observes the following rules:
   - The default view is 'feed'
   - If the view is set to 'feed', the `<Feed>` component is displayed
   - If the view is set to any other value, the `<Post>` component is displayed
   - The `changeView` function should change the value of `view` in the `App` component's state.
-
   You'll make some refactors and additions to this view switcher throughout the
   next steps of the assessment. When you're ready, return to the README.
 */
@@ -44,19 +39,32 @@ class App extends React.Component {
        ghost : false 
       },
      username : "",
-     data : []
+     password : "",
+     data : [] ,
+     showHideNav : false 
     }
-
+    this.handleChange=this.handleChange.bind(this)
     this.changeView = this.changeView.bind(this);
+    this.connect = this.connect.bind(this)
   }
+
+  handleChange(e){
+    console.log(this.state)
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+
 
   changeView(option) {
     this.setState({
      view: option
    });
  }
-connect(){
-axios.post('/api/user',this.state.userdata) 
+ connect(){
+
+axios.post('/api/user',this.state) 
    .then(data=>{
       console.log(data)
       if (data.data.username === "admin" ){
@@ -75,9 +83,12 @@ axios.post('/api/user',this.state.userdata)
          this.setState({
            userData :  {   admin : false ,
              user :true ,
-             ghost : true  },
-             username : data.data.username
+             ghost : false  
+             },
+             username : data.data,
+             showHideNav :true
          })
+         this.changeView("homePage")
         }
    })
    .catch(err=>{
@@ -90,14 +101,17 @@ axios.post('/api/user',this.state.userdata)
 renderView() {
   const {view} = this.state;
     if (view === 'preview') {
-      return <Preview data = {this.state.data} handleClick={() => this.changeView('anypostview')}/>
+
+      return <Preview data = {this.state.data} changeView={this.changeView} handleClick={() => this.changeView('anypostview')}/>
     } else if(view === 'post') {
       return <Post />
     } else if(view === 'signup'){
-      return <Signup />
+      return <Signup  />
     } else if(view === 'login'){
-      return <Login />
-    } 
+      return <Login changeView={this.changeView} handleChange={this.handleChange} connect={this.connect}/>
+    } else if( view ==='homePage'){  
+      return <HomePage  username={this.state.username} data = {this.state.data} changeView={this.changeView} handleClick={() => this.changeView('anypostview')}/>
+    }
   }
   componentDidMount(){
     this.fetch()
@@ -119,25 +133,25 @@ renderView() {
  render() {
   return (
     <div>
-      <div className="nav">
+      <div className="nav" >
         <span className="logo"
-          onClick={() => this.changeView('preview')}>
+          onClick={() => this.changeView('homePage')}>
           ARA.TN
-        </span>
+        </span>{ this.state.showHideNav &&
         <span className={this.state.view === 'preview'
           ? 'nav-selected'
           : 'nav-unselected'}
-          >
-          My Profile
-        </span>
-        <span className="nav-unselected">
+          onClick={() => this.changeView('')}>
+        My Profile
+        </span>} { this.state.showHideNav &&
+        <span className="nav-unselected" onClick={()=>this.changeView('post')}>
           Create a Post
-        </span>
-        <span className="nav-unselected">
+        </span>}{ this.state.showHideNav &&
+        <span className="nav-unselected" onClick={()=>this.changeView('preview')}>
           Log Out
-        </span>
+        </span>}
       </div>
-
+      
       <div className="main">
         {this.renderView()}
       </div>
