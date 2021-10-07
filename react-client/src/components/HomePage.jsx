c3c3294d2eeaa897f6592eac1e756cd59bf73df2
aminehajamor <amineehajamorr@gmail.com>
2021-10-07 13:14:35 +0100

    done 07.10

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import moment from 'moment'
 const HomePage = (props) => (
   <div className="feed">
+
       <br />
      <div className="wyl feed-list-item-title">What are you looking for today,  {props.username}? </div>
+     <br />
+     <select id="id_name" name="name">
+       <option value=""></option>
+    <option value="3">Houses For Rent</option>
+    <option value="4">Houses For Sale   </option>
+    <option value="5"> Cars For Rent  </option>
+    <option value="6">Cars For Sale </option>
+    <option value="7"> Phones </option>
+</select> 
+<button>filter</button>
      {/* <img className="img" src={logo2}/> */}
      <ul>
       {props.data.map((e,k)=>(
