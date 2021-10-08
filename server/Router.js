169610db8ba255e94faa7dd41e2b10aa7847f60d
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-10-08 11:45:10 +0100

    comit

--- server/Router.js
+++ server/Router.js
@@ -3,7 +3,7 @@ var Controller = require('./Controller')
 
 announceRouter.route('/announce/').post(Controller.createOne)
 announceRouter.route('/announce/').get(Controller.retrieve)
-announceRouter.route('/announce/:id').post(Controller.updateOne)
+announceRouter.route('/announce/:id').patch(Controller.updateOne)
 announceRouter.route('/announce/:id').delete(Controller.deleteOne)
 announceRouter.route('/').post(Controller.check)
 
